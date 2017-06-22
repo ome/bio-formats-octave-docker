ea157b652bbbc5fdad4365ec4de9596c73a9f927
Sebastien Besson <seb.besson@gmail.com>
2017-06-22 15:18:59 +0100

    Re-execute test in debugging mode

--- minimal_test.m
+++ minimal_test.m
@@ -1,7 +1,13 @@
 % Load Bio-Formats
 pkg load bioformats
 
+% Check the Bio-Formats version and initialize logging
 [s, v] = bfCheckJavaPath()
 bfInitLogging('INFO');
+
+test_bfopen('tubhiswt-3D/tubhiswt_C0.ome.tif');
+
+% Increase the debugging verbosity
+loci.common.DebugTools.setRootLevel('DEBUG');
 
 test_bfopen('tubhiswt-3D/tubhiswt_C0.ome.tif');
